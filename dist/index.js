859aed68d314b91caca48b433c4cda2ded943076
vincent <muehler.v@gmail.com>
2018-04-16 14:30:17 +0200

    generate index.d.ts files

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ImageDownloader_1 = require("./ImageDownloader");
+exports.ImageDownloader = ImageDownloader_1.ImageDownloader;
+//# sourceMappingURL=index.js.map
